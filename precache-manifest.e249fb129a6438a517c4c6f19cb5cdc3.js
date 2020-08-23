eefae03148fbdc3b9fd93998da58df82deabaf3d
Nick Wang <wang703003515@gmail.com>
2020-08-23 12:49:54 +0800

    Updates

---
+++ precache-manifest.e249fb129a6438a517c4c6f19cb5cdc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd1c9ff61d7bb1fb92db49c912c34916",
+    "url": "/auto-dialer/index.html"
+  },
+  {
+    "revision": "c28a8192ab1ffa7ca222",
+    "url": "/auto-dialer/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "6253ad19198188606295",
+    "url": "/auto-dialer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c28a8192ab1ffa7ca222",
+    "url": "/auto-dialer/static/js/2.17af7e1c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/auto-dialer/static/js/2.17af7e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6253ad19198188606295",
+    "url": "/auto-dialer/static/js/main.df936fba.chunk.js"
+  },
+  {
+    "revision": "33a1ea23af4adbd146fc",
+    "url": "/auto-dialer/static/js/runtime-main.9d5dee77.js"
+  }
+]);
